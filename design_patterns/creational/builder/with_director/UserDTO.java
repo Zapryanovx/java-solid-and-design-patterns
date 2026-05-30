26bdfa1ead8ec6b6404d4c920c488ae567d081cc
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-05-30 21:02:10 +0300

    add builder with director implementation

---
+++ design_patterns/creational/builder/with_director/UserDTO.java
@@ -0,0 +1,10 @@
+package design_patterns.creational.builder;
+
+public interface UserDTO {
+
+    String getName();
+
+    String getAddress();
+
+    String getAge();
+}
---
+++ design_patterns/creational/builder/with_director/UserDTOBuilder.java
@@ -0,0 +1,18 @@
+package design_patterns.creational.builder;
+
+import java.time.LocalDate;
+
+public interface UserDTOBuilder {
+
+    UserDTOBuilder withFirstName(String firstName);
+
+    UserDTOBuilder withLastName(String lastName);
+
+    UserDTOBuilder withBirthday(LocalDate date);
+
+    UserDTOBuilder withAddress(Address address);
+
+    UserDTO build();
+
+    UserDTO getUserDTO();
+}
