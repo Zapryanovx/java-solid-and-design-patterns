5b059a7766661e0e1802dc813b30522a5eab327c
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-06-05 10:27:38 +0300

    add eager singleton implementation

---
+++ design_patterns/creational/singleton/eager/Client.java
@@ -0,0 +1,12 @@
+package design_patterns.creational.singleton.eager;
+
+public class Client {
+
+	public static void main(String[] args) {
+
+		EagerRegistry registry = EagerRegistry.getInstance();
+		EagerRegistry registry2 = EagerRegistry.getInstance();
+		System.out.println(registry == registry2);
+	}
+
+}
---
+++ design_patterns/creational/singleton/eager/EagerRegistry.java
@@ -0,0 +1,16 @@
+package design_patterns.creational.singleton.eager;
+
+/**
+ * This class uses eager initialization of singleton instance.
+ */
+public class EagerRegistry {
+
+    private EagerRegistry() {
+    }
+
+    private static final EagerRegistry INSTANCE = new EagerRegistry();
+
+    public static EagerRegistry getInstance() {
+        return INSTANCE;
+    }
+}
