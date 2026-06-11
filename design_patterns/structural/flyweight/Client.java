548cf40ffa1b6ffc16e8876738b70763784df4a8
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-06-12 02:24:18 +0300

    fix typo in flyweight implementation

--- design_patterns/structural/flyweight/Client.java
+++ design_patterns/structural/flyweight/Client.java
@@ -4,7 +4,7 @@ public class Client {
 
     public static void main(String[] args) {
 
-		SystemxErrorMessage msg1 = ErrorMessageFactory.getInstance()
+		SystemErrorMessage msg1 = ErrorMessageFactory.getInstance()
 				.getError(ErrorMessageFactory.ErrorType.GenericSystemError);
 		System.out.println(msg1.getText("4056"));
 
