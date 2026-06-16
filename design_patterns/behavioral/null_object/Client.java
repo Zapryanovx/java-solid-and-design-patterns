411120830459e7b35a7fb613fbbcbf3e48c6391e
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-06-16 17:46:12 +0300

    add null object implementation

--- design_patterns/behavioral/null_object/Client.java
+++ design_patterns/behavioral/null_object/Client.java
@@ -3,7 +3,7 @@ package design_patterns.behavioral.null_object;
 public class Client {
 
     public static void main(String[] args) {
-        ComplexService service = new ComplexService("Simple report", new StorageService());
+        ComplexService service = new ComplexService("Simple report 2", new NullStorageService());
         service.generateReport();
     }
 }
--- design_patterns/behavioral/null_object/NullStorageService.java
+++ design_patterns/behavioral/null_object/NullStorageService.java
@@ -1,5 +1,9 @@
 package design_patterns.behavioral.null_object;
 
-public class NullStorageService {
+public class NullStorageService extends StorageService {
 
+    @Override
+    public void save(Report report) {
+        System.out.println("Null object save method. Doing nothing.");
+    }
 }
