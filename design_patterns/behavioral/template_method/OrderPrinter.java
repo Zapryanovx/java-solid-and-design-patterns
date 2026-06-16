0acdd8e8de539ee16a32fe433a8352da9b0a37ec
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-06-16 14:58:03 +0300

    add template method implementation

--- design_patterns/behavioral/template_method/OrderPrinter.java
+++ design_patterns/behavioral/template_method/OrderPrinter.java
@@ -1,6 +1,28 @@
 package design_patterns.behavioral.template_method;
 
-//Abstract base class defines the template method
-public class OrderPrinter {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+//Abstract base class defines the template method
+public abstract class OrderPrinter {
+
+    public final void printOrder(Order order, String filename) throws IOException {
+        try (PrintWriter out = new PrintWriter(filename)) {
+            out.println(start());
+            out.println(formatOrderNumber(order));
+            out.println(formatItems(order));
+            out.println(formatTotal(order));
+            out.println(end());
+        }
+    }
+
+    protected abstract String start();
+
+    protected abstract String formatOrderNumber(Order order);
+
+    protected abstract String formatItems(Order order);
+
+    protected abstract String formatTotal(Order order);
+
+    protected abstract String end();
 }
--- design_patterns/behavioral/template_method/TextPrinter.java
+++ design_patterns/behavioral/template_method/TextPrinter.java
@@ -2,8 +2,35 @@ package design_patterns.behavioral.template_method;
 
 import java.util.Map;
 
-//Concrete implementation. Implements steps needed..
-//..by template method
-public class TextPrinter {
+//Concrete implementation. Implements steps needed by template method
+public class TextPrinter extends OrderPrinter {
 
+    @Override
+    protected String start() {
+        return "Order Details";
+    }
+
+    @Override
+    protected String formatOrderNumber(Order order) {
+        return "Order #" + order.getId();
+    }
+
+    @Override
+    protected String formatItems(Order order) {
+        StringBuilder builder = new StringBuilder("Items\n");
+        for (Map.Entry<String, Double> entry : order.getItems().entrySet()) {
+            builder.append(entry.getKey()).append(" $").append(entry.getValue()).append("\n");
+        }
+        return builder.append("--------------------------").toString();
+    }
+
+    @Override
+    protected String formatTotal(Order order) {
+        return "Total: $" + order.getTotal();
+    }
+
+    @Override
+    protected String end() {
+        return "";
+    }
 }
